c9750bfb2aa5842966f67359c553acc27201407b
Mostafa Ramadan <96018237+mostafaramadanhamed@users.noreply.github.com>
2024-02-06 10:22:15 +0200

    Add files via upload

---
+++ app_router.dart
@@ -0,0 +1,31 @@
+import 'package:doctor_app/core/routing/routes.dart';
+import 'package:doctor_app/features/login/ui/login_screen.dart';
+import 'package:doctor_app/features/onboarding/ui/onboarding_screen.dart';
+import 'package:flutter/material.dart';
+
+class AppRouter{
+
+  Route generateRoute(RouteSettings settings){
+    switch (settings.name ){
+      case Routes.onboardingScreen :
+        return MaterialPageRoute(
+            builder: (_)=> const OnboardingScreen(),
+        );
+        case Routes.loginScreen :
+        return MaterialPageRoute(
+            builder: (_)=> const LoginScreen(),
+        );
+      default:
+        return MaterialPageRoute(
+            builder: (_)=>Scaffold(
+              body: Center(
+                child: Text(
+                  'No route defined for ${settings.name}',
+                ),
+              ),
+            ),
+        );
+    }
+  }
+
+}
